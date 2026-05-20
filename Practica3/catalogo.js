3b43feb39475c2aa6b0bd7e85e7bfaeeb97763d0
wowito68 <memohalo12@hotmail.com>
2026-05-19 19:56:24 -0600

    actualizacion del catalogo añadiendo el atributo "Disponible"

--- Practica3/catalogo.js
+++ Practica3/catalogo.js
@@ -1,81 +1,98 @@
+import { fa } from "zod/locales";
 
 export let catalogo = [
     {
         id: 1,
         nombre: "Americano",
         precio: 5,
-        categoria: "Bebida"
+        categoria: "Bebida",
+        Disponible: true
     },
     {
         id: 2,
         nombre: "Capuchino",
         precio: 8.3,
-        categoria: "Bebida"
+        categoria: "Bebida",
+        Disponible: true
+
     },
     {
         id: 3,
         nombre: "Expresso",
         precio: 7.5,
-        categoria: "Bebida"
+        categoria: "Bebida",
+        Disponible: true
     },
     {
         id: 4,
         nombre: "Latte",
         precio: 5,
-        categoria: "Bebida"
+        categoria: "Bebida",
+        Disponible: false
     },
     {
         id: 5,
         nombre: "Macchiato",
         precio: 6.2,
-        categoria: "Bebida"
+        categoria: "Bebida",
+        Disponible: true
     },
     {
         id: 6,
         nombre: "Black Coffee",
         precio: 9.5,
-        categoria: "Bebida"
+        categoria: "Bebida",
+        Disponible: true
     },
     {
         id: 7,
         nombre: "Hot chocolate",
         precio: 10,
-        categoria: "Bebida"
+        categoria: "Bebida",
+        Disponible: true
     },
     {
         id: 8,
         nombre: "Original milk",
         precio: 5,
-        categoria: "Bebida"
+        categoria: "Bebida",
+        Disponible: false
     },
     {
         id: 9,
         nombre: "Burger",
         precio: 5,
-        categoria: "Snack"
+        categoria: "Snack",
+        Disponible: true
     },
     {
         id: 10,
         nombre: "French Fries",
         precio: 5,
-        categoria: "Snack"
+        categoria: "Snack",
+        Disponible: true
+
     },
     {
         id: 11,
         nombre: "Oatmeal",
         precio: 5,
-        categoria: "Snack"
+        categoria: "Snack",
+        Disponible: false
     },
     {
         id: 12,
         nombre: "Cookies",
         precio: 5,
-        categoria: "Snack"
+        categoria: "Snack",
+        Disponible: true
+
     },
     {
         id: 13,
         nombre: "Macarons",
         precio: 5,
-        categoria: "Snack"
+        categoria: "Snack",
+        Disponible: false
     },
 ];
