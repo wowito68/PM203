3c984bab020997c45483294223145bdbba1d4552
wowito68 <memohalo12@hotmail.com>
2026-06-10 18:39:51 -0600

    Commit Practica 7

--- Practica5/my-app2/components/Perfil.js
+++ Practica5/my-app2/components/Perfil.js
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
+import { View, Text,Button } from 'react-native'
+import React,{ useState } from 'react'
 
-export const Perfil = () => {
+export const Perfil = (nombre,carrera,materia,cuatrimestre) => {
+    const[mostrar,setMostrar] = useState(false)
+
     return (
         <View>
-            <Text>Nombre: Guillermo Alvarez Sanchez</Text>
-            <Text>Carrera: ING Sistemas</Text>
-            <Text>Materia: Programacion Movil</Text>
-            <Text>Cuatrimestre: 9no</Text>
+            <Text>Hola {nombre}</Text>
+
+            {/* Renderizado condicional */}
+            {mostrar &&
+                <> 
+                    <Text>Carrera: {carrera}</Text>
+                    <Text>Materia: {materia}</Text>
+                    <Text>Cuatrimestre: {cuatrimestre}</Text>
+                </>
+            }
+
+            <Button title="Mostrar perfil" onPress={() => setMostrar(!mostrar)}></Button>
         </View>
     )
 }
+
+
+/*
+import { View, Text,Button } from 'react-native'
+
+export const Perfil = (props) => {
+    return (
+        <View>
+            <Text>Hola {props.nombre}</Text>
+            <Text>Carrera: {props.carrera}</Text>
+            <Text>Materia: {props.materia}</Text>
+            <Text>Cuatrimestre: {props.cuatrimestre}</Text>
+        </View>
+    )
+}
+*/
