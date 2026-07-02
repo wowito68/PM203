abd336337497656fad362be023249c0bf53a51b8
wowito68 <memohalo12@hotmail.com>
2026-07-01 18:19:53 -0600

    Practica No.16: ActivityIndicator, KeyboardAvoidingView

--- Practica5/my-app2/screens/ActivityIndicatorScreen.js
+++ Practica5/my-app2/screens/ActivityIndicatorScreen.js
@@ -1,28 +1,92 @@
-// zona1: importaciones de componetes y archivos
+/*Zona 1: Importaciones de componentes y archivos */
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  Text,
+  Platform,
+  TouchableWithoutFeedback,
+  Button,
+  Keyboard,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
 
-import { StatusBar } from 'expo-status-bar'; //puede ser opcion quitarlo o no
-import { StyleSheet, Text, View, } from 'react-native';
-
-
-// zona2 main aquí van los componetes
+/*Zona 2: Main - Hogar de los componentes */
 export default function ActivityIndicatorScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Aqui va la practica de Ana </Text>
-      <StatusBar style="auto" />
+  const [cargando, setCargando] = useState(false);
 
-    </View>
+  const handleSubmit = () => {
+    Keyboard.dismiss();
+    setCargando(true);
+    setTimeout(() => {
+      setCargando(false);
+    }, 2000);
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.screen}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}
+      enabled={true}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Text style={styles.header}>Header</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+          />
+          {cargando && (
+            <ActivityIndicator
+              animating={true}
+              size="large"
+              color="green"
+              style={styles.spinner}
+            />
+          )}
+          <View style={styles.buttonWrapper}>
+            <Button title="Submit" onPress={handleSubmit} />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
-// Zona 3 Estilos y Posicionamiento
+/*Zona 3: Estilos y Posicionamiento */
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    flexDirection: 'row'
   },
 
+  container: {
+    padding: 24,
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+
+  header: {
+    fontSize: 36,
+    marginBottom: 48,
+  },
+
+  input: {
+    height: 40,
+    borderColor: '#000000',
+    borderBottomWidth: 1,
+    marginBottom: 36,
+  },
+
+  spinner: {
+    marginVertical: 12,
+  },
+
+  buttonWrapper: {
+    backgroundColor: 'white',
+    marginTop: 12,
+  },
 });
